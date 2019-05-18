40eb190e9f2e39ae9d08ac95ff692a84080044d2
nayan19997 <nayan@elpuig.xeill.net>
2019-05-18 09:44:57 +0200

    histori1

--- app/src/main/java/com/example/dukusho_nv/model/Page.java
+++ app/src/main/java/com/example/dukusho_nv/model/Page.java
@@ -3,6 +3,7 @@ package com.example.dukusho_nv.model;
 public class Page {
     public String image;
     public String text;
+    public String id;
     public Option option1;
     public Option option2;
 }
